49b7c9db9fcb9a4b61b5e912d3792bf472b508e6
nikolicDjordje <84343151+nikolicDjordje@users.noreply.github.com>
2021-06-02 18:08:08 +0200

    Add files via upload

---
+++ Lokatori.java
@@ -0,0 +1,19 @@
+package vezba;
+
+public class Lokatori {
+	
+	public static final String HEADER_XPATH = "/html/body/div[1]/div[2]/nav";
+	public static final String ADRESS_NAME_LOCATOR = "adress1";
+	public static final String COUNTRY_FULLXPATH = "/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[11]/td[2]/select";
+	public static final String COUNTRY_XPATH = "/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[11]/td[2]/select";
+	public static final String USERNAME_XPATH = "//*[@id=\"email\"]";
+	public static final String USERNAME_FULLXPATH = "/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[13]/td[2]/input";
+	public static final String USERNAME_ID = "email";
+	
+
+	public static void main(String[] args) {
+
+	}
+
+	
+}
